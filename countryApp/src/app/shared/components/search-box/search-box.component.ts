e83bc08d0e7c791ff5ae14c3bba953e535b6359b
mariovillar <mario.villar@nter.es>
2024-05-28 08:19:02 +0200

    countries

---
+++ countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrl: './search-box.component.css',
+})
+export class SearchBoxComponent {
+  @Input()
+  public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+
+
+
+
+
+  }
+
+
+
+
+
+}
