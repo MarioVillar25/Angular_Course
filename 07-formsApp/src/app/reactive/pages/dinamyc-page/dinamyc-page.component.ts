5b1032e7646eec183097a1c0a5fe91f24426d118
mariovillar <mario.villar@nter.es>
2024-06-07 09:52:42 +0200

    formularios reactivos

--- 07-formsApp/src/app/reactive/pages/dinamyc-page/dinamyc-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dinamyc-page/dinamyc-page.component.ts
@@ -1,10 +1,91 @@
 import { Component } from '@angular/core';
+import {
+  Form,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dinamyc-page',
   templateUrl: './dinamyc-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class DinamycPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favouriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death strundun', Validators.required],
+    ]),
+  });
 
+  public newFavorite: FormControl = new FormControl('', Validators.required);
+
+  constructor(private fb: FormBuilder) {}
+
+  //GETTER para favouriteGames
+
+  get favGames() {
+    return this.myForm.get('favouriteGames') as FormArray;
+  }
+
+  //Funciones para validaciones
+
+  isValidFieldInArray(FormArray: FormArray, i: number) {
+    return FormArray.controls[i].errors && FormArray.controls[i].touched;
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+
+    return null;
+  }
+
+  onAddToFavorites(): void {
+    if (this.newFavorite.invalid) return;
+
+    console.log(this.newFavorite.value);
+
+    const newGame = this.newFavorite.value;
+
+    this.favGames.push(this.fb.control(newGame, Validators.required));
+
+    this.newFavorite.reset();
+  }
+
+  onDeleteFavorite(i: number): void {
+    this.favGames.removeAt(i);
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    (this.myForm.controls['favouriteGames'] as FormArray) = this.fb.array([])
+    this.myForm.reset();
+  }
 }
--- 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SwitchesPageComponent {
+  //declaramos el FormGroup
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  //Funciones de Validaciones:
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  //Función onSubmit:
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+
+      return;
+    }
+
+    console.log(this.myForm.value);
+  }
 }
