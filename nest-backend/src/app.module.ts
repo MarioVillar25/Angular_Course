d4a5fb85c45a58ea29796dd6a05f81e885b67e2f
mariovillar <mario.villar@nter.es>
2024-06-14 12:03:22 +0200

    backend nest

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,10 +1,17 @@
+/* eslint-disable prettier/prettier */
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), //Para tener acceso a nuestras variables de entorno
+    MongooseModule.forRoot(process.env.MONGO_URI), //Para tener acceso a la base de datos
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ nest-backend/src/auth/interfaces/login-response.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+
+import { User } from "../entities/user.entity";
+
+
+export interface LoginResponse {
+
+    user: User;
+    token: string;
+
+}
